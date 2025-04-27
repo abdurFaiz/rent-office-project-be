b50040de76ea4afdfe95f37a5523be260f38f6b9
abdurFaiz <115395387+abdurFaiz@users.noreply.github.com>
2025-04-27 20:28:10 +0700

    Implement City and OfficeSpace controllers with resource responses and API key middleware

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CityController;
+use App\Http\Controllers\Api\OfficeSpaceController;
+use App\Http\Controllers\Api\BookingTransactionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/city/{city:slug}', [CityController::class, 'show']);
-Route::apiResource('/cities', [CityController::class]);
+Route::middleware('api_key')->group(function () {
 
-Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
-Route::apiResource('/offices', [OfficeSpaceController::class]);
+    // sedangkan route ini hanya membackup method show dan biasanya digunakan untuk task spesifik
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
 
-Route::get('/booking-transaction', [BookingTransactionController::class, 'store']);
-Route::apiResource('/check-booking', [BookingTransactionController::class, 'booking_details']);
+    // api resource ini sudah membackup semua method yg ada paada controller
+    Route::apiResource('/cities', CityController::class);
+
+    Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/offices', OfficeSpaceController::class);
+
+    Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
+    Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
+});
